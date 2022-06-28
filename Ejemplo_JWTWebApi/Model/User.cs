12bdeedd0d4194527ee9074c34c27a7af6c94b35
AlanSforzini <alan.sforzini@globallogic.com>
2022-06-28 16:09:54 -0300

    Add project files.

---
+++ Ejemplo_JWTWebApi/Model/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Ejemplo_JWTWebApi.Model
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
